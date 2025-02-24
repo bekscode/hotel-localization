cc6491c8ec68f9ccee148cf48cef2cc07a4b8735
Rebekah Romang <102334386+bekscode@users.noreply.github.com>
2025-02-24 01:21:44 -0500

    B1b Display the welcome message in both English and French by applying the resource bundles using a different thread for each language
    Created files: DisplayMessage.java and WelcomeController.java
    
    Created and started threads for the English and French welcome messages and added HTML elements to display the welcome messages on the landing page

--- src/main/java/edu/wgu/d387_sample_code/D387SampleCodeApplication.java
+++ src/main/java/edu/wgu/d387_sample_code/D387SampleCodeApplication.java
@@ -1,15 +1,29 @@
 package edu.wgu.d387_sample_code;
 
+import edu.wgu.d387_sample_code.i18n.DisplayMessage;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class D387SampleCodeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(D387SampleCodeApplication.class, args);
+
+		// Create instances for required locales
+		DisplayMessage displayMessageEnglish = new DisplayMessage(Locale.US);
+		DisplayMessage displayMessageFrench = new DisplayMessage(Locale.CANADA_FRENCH);
+
+		// Create threads for DisplayMessage instance
+		Thread englishThread = new Thread(displayMessageEnglish);
+		Thread frenchThread = new Thread(displayMessageFrench);
+
+		// Start threads
+		englishThread.start();
+		frenchThread.start();
+
 	}
 
 }
